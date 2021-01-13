d63b1195ab39e9917f1351eb882025c8bfe4fabe
CrisKLP <klepadlo.krzysztof@gmail.com>
2021-01-13 17:23:25 +0100

    Moved Store Provider to Root page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './views/Root';
-import { Provider } from 'react-redux';
-import configureStore from './data/store/';
-
-const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <Root />
-    </Provider>
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,24 +1,30 @@
-import MainTemplate from '../templates/MainTemplate';
+import { Provider } from 'react-redux';
 import {
   BrowserRouter as Router,
-  Switch,
-  Route,
   Redirect,
+  Route,
+  Switch,
 } from 'react-router-dom';
+import configureStore from '../data/store/';
+import MainTemplate from '../templates/MainTemplate';
 import Home from './Home';
 import Job from './Job';
 
+const store = configureStore();
+
 const Root = () => {
   return (
-    <Router>
-      <Switch>
-        <MainTemplate>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/job/:id" component={Job} />
-          <Redirect to="/" />
-        </MainTemplate>
-      </Switch>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Switch>
+          <MainTemplate>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/job/:id" component={Job} />
+            <Redirect to="/" />
+          </MainTemplate>
+        </Switch>
+      </Router>
+    </Provider>
   );
 };
 
